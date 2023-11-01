6bf45515accbe3e5d5b479a4fcae848aba196bc7
newangry <yevhenii.yevtushenko1@gmail.com>
2023-11-01 17:02:59 -0300

    chagend user type

--- entities/user.py
+++ entities/user.py
@@ -5,6 +5,7 @@ from datetime import date, time
 class Users(BaseModel):
     ID: Optional[int] = None
     Email: Optional[str] = None
+    UserType: Optional[str] = None
     Password: Optional[str] = None
     AccessToken: Optional[str] = None
     AccessTokenCounter: Optional[int] = None
